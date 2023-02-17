73d8512b55991aa578de28135bb2539a71a25888
Dennis Every <dennis@ansyra.com>
2023-02-17 18:23:21 -0400

    Reuse custom hook with Modal

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,11 @@
 import Header from './Header';
+import Modal from './Modal';
 import TodosLogic from './TodosLogic';
 
 const TodoApp = () => (
   <section id="todos-section">
     <Header />
+    <Modal />
     <TodosLogic />
   </section>
 );
