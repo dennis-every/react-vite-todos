73ebdcc3ff9cdc46d31fd6553fab226218b6f58c
Dennis Every <dennis@ansyra.com>
2023-02-17 13:02:23 -0400

    Add index.css and support for absolute import paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import TodoApp from './components/TodoApp';
+import TodoApp from '@/components/TodoApp';
 
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
