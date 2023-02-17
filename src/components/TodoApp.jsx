36ee3e3ffef4eb9383ebab55e894c074385c1fa4
Dennis Every <dennis@ansyra.com>
2023-02-17 14:24:49 -0400

    Center todos and add some styles

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,9 +2,9 @@ import Header from './Header';
 import TodosLogic from './TodosLogic';
 
 const TodoApp = () => (
-  <>
+  <section id="todos-section">
     <Header />
     <TodosLogic />
-  </>
+  </section>
 );
 export default TodoApp;
