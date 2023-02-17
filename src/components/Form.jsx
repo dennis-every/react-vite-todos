76d255304f9c644606f2cc5690ac4510d7c04c5d
Dennis Every <dennis@ansyra.com>
2023-02-17 18:54:33 -0400

    Get data from uncontroller input using useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,26 @@
-const Form = () => (
-  <>
-    <h1>React Form Handling</h1>
-    <form>
-      <label htmlFor="name">
-        First Name:
-        {' '}
-        <input name="name" type="text" defaultValue="Enter name..." />
-      </label>
-    </form>
-  </>
-);
+import { useRef } from 'react';
+
+const Form = () => {
+  const ref = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // eslint-disable-next-line no-alert
+    alert(ref.current.value);
+  };
+
+  return (
+    <>
+      <h1>React Form Handling</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          First Name:
+          {' '}
+          <input name="name" type="text" ref={ref} defaultValue="Enter name..." />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
 
 export default Form;
