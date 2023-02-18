2d972e5f5175e081b276db90860cd4cedf15d6f2
Dennis Every <dennis@ansyra.com>
2023-02-18 14:27:21 -0400

    Add input todo component

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,2 +1,8 @@
-const InputTodo = () => <div>input field here...</div>;
+const InputTodo = () => (
+  <form>
+    <input type="text" placeholder="Add todo..." />
+    <button type="submit">Submit</button>
+  </form>
+);
+
 export default InputTodo;
