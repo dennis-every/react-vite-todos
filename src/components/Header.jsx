07a91678e906456931e8e4560c993503bafb9b9b
Dennis Every <dennis@ansyra.com>
2023-02-18 15:53:10 -0400

    Add inline styles with Header style object

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
-const Header = () => (
-  <header>
-    <h1>todos</h1>
-    <p>Items will persist in the browser local storage</p>
-  </header>
-);
+const Header = () => {
+  const headerStyle = {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+    color: '#aeadad',
+    textAlign: 'center',
+  };
+  return (
+    <header style={headerStyle}>
+      <h1>todos</h1>
+      <p>Items will persist in the browser local storage</p>
+    </header>
+  );
+};
 
 export default Header;
